1451f760daad7ecbc1ba15de59431510d04415ea
jwlsiu <114306901+jwlsiu@users.noreply.github.com>
2023-04-22 19:21:30 +0100

    Update Joins.sql

--- Joins/Joins.sql
+++ Joins/Joins.sql
@@ -1,61 +1,18 @@
--- Filtering Data Part 02 – Exercise 1
--- Display employees whose name begins with 'P' and whose salary is below 8000.
-SELECT first_name, last_name, salary
-FROM employees
-WHERE first_name LIKE 'P%'
-AND salary < 8000;
+-- Joins – Exercise 1
+-- Create an sql that will display the following fields (attributes) – first name, last name, manager_id, department_id, location_id. Using an EQUI join display the data with an appropriate query.
+SELECT first_name, last_name, manager_id, e.department_id, d.location_id
+FROM employees AS e
+INNER JOIN departments AS d ON e.department_id = d.department_id
+INNER JOIN locations AS l ON l.location_id = d.location_id;
 
--- Filtering Data Part 02 – Exercise 2
--- Identify employees hired in 1997, 1998 with a department id of 5.
-SELECT first_name, last_name, hire_date, department_id
-FROM employees
-WHERE hire_date
-BETWEEN '1997-01-01'
-AND '1998-12-3'
-AND department_id = 5;
+-- Joins – Exercise 2
+-- Create an sql that will display the following fields (attributes) – location_id, postal_code, country_id, country_name – apply a series of alias’ to the relevant tables. Then generate the output according to an INNER JOIN. With the primary key ‘location_id’ deciding what will be the prevalent table in the query.
+SELECT location_id, postal_code, c.country_id, country_name
+FROM locations l
+INNER JOIN countries c ON c.country_id = l.country_id;
 
--- Filtering Data Part 02 – Exercise 3
--- Display employees who were employed in 1998, 1999, 2000. Display their first name, last name, salary, email and hire date (alias 'Year of Hire') and order by last name is ascending order.
-SELECT first_name, last_name, salary, email, hire_date AS 'Year of Hire'
-FROM employees
-WHERE YEAR ( hire_date )
-IN ( 1998, 1999, 2000 )
-ORDER BY last_name;
-
--- Filtering Data Part 02 – Exercise 4
--- Display all details of employees that have a salary bounded between 5000 and 10000 (inclusive). 
-SELECT *
-FROM employees
-WHERE salary >= 5000
-AND salary <= 10000
-ORDER BY salary DESC;
-
--- Filtering Data Part 02 – Exercise 5
--- Identify all employees that have salaries between 4000 and 12000. In ascending order according to first name.
-SELECT first_name, last_name, salary
-FROM employees
-WHERE salary
-BETWEEN 4000
-AND 12000
-ORDER BY first_name ASC;
-
--- Filtering Data Part 02 – Exercise 6
--- Display all the records for employees that are NOT in the following departments. They are not in 1, 4, 8, 10. So it will display all the others. Sort according to salary in descending order.
-SELECT *
-FROM employees
-WHERE department_id NOT
-IN (1, 4, 8, 10)
-ORDER BY salary DESC;
-
--- Filtering Data Part 02 – Exercise 7
--- Display the details of the employees that have an ‘s’ at the end of their first name. Sort using ascending sort according to first name
-SELECT first_name, last_name
-FROM employees
-WHERE first_name LIKE '%s'
-ORDER BY first_name ASC;
-
--- Filtering Data Part 02 – Exercise 8
--- Display the details of employees that have three letters unknown followed by a ‘g’  for their last name.
-SELECT employee_id, first_name, last_name
-FROM employees
-WHERE last_name LIKE '___g';
+-- Joins – Exercise 3
+-- Create a query that will generate the following information, employee_id, first_name, last_name, dependent_id, first_name, last_name, relationship. This will draw information from the employees and dependents tables.
+SELECT e.employee_id, e.first_name, e.last_name, dependent_id, d.first_name, d.last_name
+FROM employees e
+INNER JOIN dependents d ON e.employee_id = d.employee_id;
